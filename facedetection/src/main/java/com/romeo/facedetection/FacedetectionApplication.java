f83d22c8b0617ef59d994d8aa35c207094c239e8
Romeo Lyams <romeolyams383@gmail.com>
2026-09-24 20:23:05 +0100

    Initial commit: Spring Boot project structure for Face Detection

---
+++ facedetection/src/main/java/com/romeo/facedetection/FacedetectionApplication.java
@@ -0,0 +1,13 @@
+package com.romeo.facedetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacedetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacedetectionApplication.class, args);
+	}
+
+}
